5134c18c71cfb88c41e9d7f30359227e004b50d0
xonlly <ariege.creation@gmail.com>
2016-09-06 22:25:34 +0200

    :wine_glass:  Live unzip gz file on download

--- src/getter/roads/item_history.js
+++ src/getter/roads/item_history.js
@@ -1,6 +1,5 @@
 
 import base     from './base'
-import targz    from 'targz'
 import Region   from './region_id'
 import request  from 'request'
 import fs       from 'fs'
@@ -8,6 +7,7 @@ import path     from 'path'
 import progress from 'request-progress'
 
 const wait = delay => new Promise(resolve => setTimeout( resolve(), delay ))
+const zlib = require('zlib');
 
 export default class ItemHistory extends base {
 
@@ -31,22 +31,6 @@ export default class ItemHistory extends base {
         return `http://eve-marketdata.com/developers/mysql_items_history_30_${ id }.txt.gz`
     }
 
-    unGz( src, dest ) {
-        return new Promise((resolve, reject) => {
-            targz.decompress({
-                src : path.join( __dirname, 'tempory', src),
-                dest : path.join( __dirname, 'tempory' )
-            }, err => {
-                if ( err )
-                    return reject( err )
-
-                this.debug.roads('item history', src, 'finish untar')
-
-                return resolve()
-            })
-        })
-    }
-
     download( r ) {
         return new Promise((resolve, reject) =>
             progress(request( r.link ))
@@ -57,17 +41,22 @@ export default class ItemHistory extends base {
 
                 .on('error', err => reject( err ))
 
-                .pipe( fs.createWriteStream( path.join( __dirname, 'tempory', `${ r.id }.txt.gz` ) ) )
+                .pipe( zlib.Gunzip() )
+
+                .pipe( fs.createWriteStream( path.join( __dirname, 'tempory', `${ r.id }.txt` ) ) )
 
                 .on('finish', () => {
                     this.debug.roads('item history', r.id, 'finish download')
                     resolve( r )
                 })
+
+                .on('error', err => reject( err ))
         )
     }
 
-    insert( r ) {
+    insert( r, files ) {
         return new Promise(( resolve, reject ) => {
+            console.log('cou', files)
             /* Work in progress ... */
         })
     }
@@ -76,16 +65,17 @@ export default class ItemHistory extends base {
 
         let tasks = Promise.resolve()
 
-        ;( regions || this.region.get() ).map( r => ({ id : r, link : this.getLinkRegion( r ) }) )
+        ;( regions || this.region.get() )
+            .map( r => ({ id : r, link : this.getLinkRegion( r ) }) )
             .forEach( r => {
 
                 this.debug.roads('prepare', r.link)
 
                 tasks = tasks
-                    .then(() => this.download( r ))
-                    .then(() => wait( 100 )) /* wait file close cursor */
-                    .then(() => this.unGz(`${ r.id }.txt.gz`))
-                    .then(() => this.insert())
+                    .then(() => this.download( r )) // For tests this line is comment ( no spam download on source )
+                    .then(() => wait( 500 )) /* wait file close cursor */
+                    //.then(() => this.unGz(`${ r.id }.txt.gz`))
+                    .then(files => this.insert( r, files ))
 
             } )
 
