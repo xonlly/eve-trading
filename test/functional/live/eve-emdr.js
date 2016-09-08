c3aec008d4712c44ba6272190302f7b73db6aa31
xonlly <ariege.creation@gmail.com>
2016-09-08 21:12:06 +0200

    Test

--- test/functional/live/eve-emdr.js
+++ test/functional/live/eve-emdr.js
@@ -1,11 +1,9 @@
 
 import live from '../../../src/getter/live'
 
+import expect from 'expect'
+
 describe('eve-emdr',function(){
-
-    beforeEach(function () {
-
-    })
 
     it('init', function () {
         return live.init()
@@ -15,6 +13,22 @@ describe('eve-emdr',function(){
         return live.disconnect()
     })
 
+    it('try parse data', function ( done ) {
 
+        let isDone = false
+
+        live.init()
+        live.listen( r => {
+
+            if ( r.length && !isDone ) {
+
+
+                live.disconnect()
+                isDone = true
+                done()
+            }
+
+        } )
+    })
 
 })
